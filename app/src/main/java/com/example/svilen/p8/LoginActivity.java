d93e094d1fc2e7340dcca3fc2be36dc71f24e2b7
EmilSiegenfeldt <Riddle1234>
2016-03-13 16:20:26 +0100

    Cleaned up project

---
+++ app/src/main/java/com/example/svilen/p8/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.svilen.p8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("....","....");
+    }
+}
--- app/src/main/java/com/example/svilen/p8/StudentActivity.java
+++ app/src/main/java/com/example/svilen/p8/StudentActivity.java
@@ -2,18 +2,12 @@ package com.example.svilen.p8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d("....","....");
-    }
-
-    public static void testMethod(){
-
+        setContentView(R.layout.activity_student);
     }
 }
