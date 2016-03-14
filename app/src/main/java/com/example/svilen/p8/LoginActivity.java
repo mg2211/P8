c2f0a2813134ea727af61021e349155f7711428e
EmilSiegenfeldt <Riddle1234>
2016-03-14 12:35:23 +0100

    Login screen layout

--- app/src/main/java/com/example/svilen/p8/LoginActivity.java
+++ app/src/main/java/com/example/svilen/p8/LoginActivity.java
@@ -2,8 +2,6 @@ package com.example.svilen.p8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
