da04e52180d3b143375bf45f7fd80cda3239d7e7
EmilSiegenfeldt <Riddle1234>
2016-03-14 12:36:35 +0100

    Login screen layout

--- app/src/main/java/com/example/svilen/p8/LoginActivity.java
+++ app/src/main/java/com/example/svilen/p8/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.svilen.p8;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,6 +10,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.d("...", "....");
     }
 
 
