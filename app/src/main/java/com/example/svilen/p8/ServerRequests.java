8396b794ed921d4de80bb8c0740aeb5a7ec3fa02
Ivo <3034@ivo>
2016-03-31 11:37:08 +0200

    Removed class DBtask which should never have been pushed to ServerRequests.

--- app/src/main/java/com/example/svilen/p8/ServerRequests.java
+++ app/src/main/java/com/example/svilen/p8/ServerRequests.java
@@ -37,98 +37,6 @@ public class ServerRequests {
         progressDialog.show();
     }
 
-    public class dbTask extends AsyncTask<String, Void, HashMap<String, String>> {
-
-        @Override
-        protected HashMap<String, String> doInBackground(String... userdata) {
-            String username = userdata[0];
-            String password = userdata[1];
-            String name = userdata[2];
-
-            String generalResponse = null;
-            int responseCode = 0;
-
-
-            try {
-                URL url = new URL("http://emilsiegenfeldt.dk/p8/newUser.php");
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-
-                Uri.Builder builder = new Uri.Builder().appendQueryParameter("username", username)
-                        .appendQueryParameter("password", password)
-                        .appendQueryParameter("name", name);
-
-                String query = builder.build().getEncodedQuery();
-                OutputStream os = connection.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-
-                connection.connect();
-
-                //Catch server response
-                InputStream in = new BufferedInputStream(connection.getInputStream());
-
-                String response = IOUtils.toString(in, "UTF-8"); //convert to readable string
-
-                //convert to JSON object
-                JSONObject JSONResult = new JSONObject(response);
-
-                //extract variables from JSONObject result var
-                generalResponse = JSONResult.getString("generalResponse");
-                responseCode = JSONResult.getInt("responseCode");
-                username = JSONResult.getString("username");
-
-                Log.d("response",response);
-
-            } catch (IOException e) {
-                responseCode = 300;
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            HashMap<String, String> result = new HashMap<>();
-
-            result.put("generalResponse", generalResponse);
-            result.put("responseCode", String.valueOf(responseCode));
-            result.put("username",username);
-            result.put("name",name);
-
-
-
-            return result;
-        }
-
-        protected void onPostExecute(HashMap<String, String> result){
-
-            progressDialog.dismiss();
-            String responseCode = result.get("responseCode");
-            String generalResponse = result.get("generalResponse");
-            String username = result.get("username");
-            String name = result.get("name");
-
-            if(Integer.parseInt(responseCode) == 100){
-                //if everything is alright
-                int duration = Toast.LENGTH_LONG;
-                Toast toast = Toast.makeText(context, generalResponse, duration);
-                toast.show();
-            } else if(Integer.parseInt(responseCode) == 200){
-                //if somethings wrong e.g. username already in use
-                int duration = Toast.LENGTH_LONG;
-                Toast toast = Toast.makeText(context, generalResponse, duration);
-                toast.show();
-            } else if(Integer.parseInt(responseCode) == 300){
-                //If server connection fails.
-                int duration = Toast.LENGTH_LONG;
-                CharSequence alert = "Server connection failed - Please try again later";
-                Toast toast = Toast.makeText(context, alert, duration);
-                toast.show();
-            }
-
-        }
-    }
-
     public class loginTask extends AsyncTask<String, Void, HashMap<String, String>> {
 
         @Override
@@ -325,4 +233,4 @@ public class ServerRequests {
         }
     }
 
-    }
+   }
