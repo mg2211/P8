a697ebca4ab1b3120d9d07c0bee43821d39db758
mg2211 <s.kondov@yahoo.com>
2016-04-06 11:25:43 +0200

    Creating new branch

--- app/src/main/java/com/example/svilen/p8/StudentActivity.java
+++ app/src/main/java/com/example/svilen/p8/StudentActivity.java
@@ -1,7 +1,10 @@
 package com.example.svilen.p8;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,7 +15,6 @@ public class StudentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student);
         setContentView(R.layout.activity_student);
 
         bLogout = (Button) findViewById(R.id.bLogout);
