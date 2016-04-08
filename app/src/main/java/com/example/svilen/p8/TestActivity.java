d4b3586b77837a4ead63e60a68c70ff929228ac7
EmilSiegenfeldt <siegen@siegen.dk>
2016-04-08 14:38:13 +0200

    UI - programmatically setting height of buttons in menu. Still minor errors.

--- app/src/main/java/com/example/svilen/p8/TestActivity.java
+++ app/src/main/java/com/example/svilen/p8/TestActivity.java
@@ -1,25 +1,109 @@
 package com.example.svilen.p8;
 
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        BarChart testChart = new BarChart(this);
-        RelativeLayout content = (RelativeLayout) findViewById(R.id.chartLayout);
-        testChart.setBackgroundColor(Color.BLUE);
-        testChart.setLogEnabled(true);
-        content.addView(testChart);
+        LinearLayout menuLinear = (LinearLayout) findViewById(R.id.menuLinear);
+
+        ArrayList<Integer> buttonIds = new ArrayList<>();
+        buttonIds.add(R.id.bClasses);
+        buttonIds.add(R.id.bAssignments);
+        buttonIds.add(R.id.bLogOut);
+        buttonIds.add(R.id.bUsers);
+        buttonIds.add(R.id.bTexts);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int height = size.y;
+
+        for(int i = 0; i < buttonIds.size(); i++){
+            Button button = (Button) findViewById(buttonIds.get(i));
+            int buttonHeight = height/buttonIds.size();
+            Log.d("buttonheight", String.valueOf(buttonHeight));
+            Log.d("button", button.toString());
+            button.setHeight(buttonHeight);
+            Log.d("new height", String.valueOf(button.getHeight()));
+        }
+
+        BarChart mChart = (BarChart) findViewById(R.id.chart);
+        mChart.setDescription("");
+
+        // if more than 60 entries are displayed in the chart, no values will be
+        // drawn
+        mChart.setMaxVisibleValueCount(60);
+
+        // scaling can now only be done on x- and y-axis separately
+        mChart.setPinchZoom(false);
+
+        mChart.setDrawBarShadow(false);
+        mChart.setDrawGridBackground(false);
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setSpaceBetweenLabels(0);
+        xAxis.setDrawGridLines(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+
+        // add a nice and smooth animation
+        mChart.animateY(2500);
+
+        mChart.getLegend().setEnabled(false);
+
+
+        ArrayList<BarEntry> yVals1 = new ArrayList<BarEntry>();
+
+        for (int i = 0; i < 10 + 1; i++) {
+            float mult = 1;
+            float val1 = (float) (Math.random() * mult) + mult / 3;
+            yVals1.add(new BarEntry((int) val1, i));
+        }
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        for (int i = 0; i < 10 + 1; i++) {
+            xVals.add((int) yVals1.get(i).getVal() + "");
+        }
+
+        BarDataSet set1 = new BarDataSet(yVals1, "Data Set");
+        set1.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        set1.setDrawValues(false);
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<IBarDataSet>();
+        dataSets.add(set1);
+
+        BarData data = new BarData(xVals, dataSets);
+
+        mChart.setData(data);
 
 
     }
