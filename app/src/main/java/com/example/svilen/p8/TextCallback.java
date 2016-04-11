681af4aa003b58432d320638eb221d2c6b6a6020
Brandur <brandur22@hotmail.com>
2016-04-11 10:36:24 +0200

    text

---
+++ app/src/main/java/com/example/svilen/p8/TextCallback.java
@@ -0,0 +1,10 @@
+package com.example.svilen.p8;
+
+import java.util.HashMap;
+
+/**
+ * Created by Brandur on 4/10/2016.
+ */
+public interface TextCallback {
+    void textListDone(HashMap<String, HashMap<String, String>> texts);
+}
