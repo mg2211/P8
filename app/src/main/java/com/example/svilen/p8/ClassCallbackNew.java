15ff75823b222815828b55d6585022ba4838a4f8
Ivo <3034@ivo>
2016-05-09 17:48:37 +0200

    added listview for displaying classes in UserActivity - is buggy.

---
+++ app/src/main/java/com/example/svilen/p8/ClassCallbackNew.java
@@ -0,0 +1,8 @@
+package com.example.svilen.p8;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface ClassCallbackNew {
+    void classListDone(Map<String, HashMap<String, String>> classes);
+}
