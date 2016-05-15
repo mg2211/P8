316d591d1a310c70ba5736c8d381a329e2ed71f2
Brandur <brandur22@hotmail.com>
2016-05-15 22:10:06 +0200

    Reading activity - with totaltimespent

--- app/src/main/java/com/example/svilen/p8/ReadingActivity.java
+++ app/src/main/java/com/example/svilen/p8/ReadingActivity.java
@@ -7,19 +7,16 @@ import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -102,6 +99,7 @@ public class ReadingActivity extends AppCompatActivity  {
             assignmentName = (String) b.get("assignmentName");
             tvAssignmentName.setText(assignmentName);
             assignmentId = (String) b.get("id");
+            Log.d("7878", assignmentId);
         }
 
         chronometer.setBase(SystemClock.elapsedRealtime()); // sets the base for the clock
@@ -420,7 +418,7 @@ public class ReadingActivity extends AppCompatActivity  {
                             public void questresultdone(HashMap<String, HashMap<String, String>> questresult) {
 
                             }
-                        }, context).execute(assignmentId, lastElement, "", answerIdtoChosenAnswer, "1", "1", "1","insert");
+                        }, context).execute(assignmentId, lastElement, "", answerIdtoChosenAnswer, "1", "1", "1","insert"); // remember to change isCompletet to empty when done!!
 
                     } else {
                         Log.d("YOU HAVE ANSWERED ", "INCORRECT!");
@@ -431,7 +429,8 @@ public class ReadingActivity extends AppCompatActivity  {
                             public void questresultdone(HashMap<String, HashMap<String, String>> questresult) {
 
                             }
-                        }, context).execute(assignmentId, lastElement, "", answerIdtoChosenAnswer, "0", "1", "1","insert");
+                        }, context).execute(assignmentId, lastElement, "", answerIdtoChosenAnswer, "0", "1", "1","insert");// remember to change isCompletet to empty when done!!
+
                     }
                     Log.d("STUDENTANSWER: ", correctOrNot.toString());
 
@@ -468,7 +467,7 @@ public class ReadingActivity extends AppCompatActivity  {
                             public void questresultdone(HashMap<String, HashMap<String, String>> questresult) {
 
                             }
-                        }, context).execute(assignmentId, "", "", "", "", "", totalSeconds,"insert");
+                        }, context).execute(assignmentId, "", "", "", "", "1", totalSeconds,"insert");
                     }
 
 
