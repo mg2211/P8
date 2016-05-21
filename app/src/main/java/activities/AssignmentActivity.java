c72b0966d982a654e5aff4c134e591de7e3b906f
Ivo <3034@ivo>
2016-05-21 17:03:44 +0200

    Renamed QuestionListAdapter to original value.

--- app/src/main/java/activities/AssignmentActivity.java
+++ app/src/main/java/activities/AssignmentActivity.java
@@ -59,7 +59,7 @@ import java.util.concurrent.TimeoutException;
 import callback.Callback;
 import callback.DatePickerCallback;
 import helper.AssignmentListAdapter;
-import helper.SomethingElseEntirely;
+import helper.QuestionListAdapter;
 import helper.UserInfo;
 import serverRequests.AssignmentLibTask;
 import serverRequests.AssignmentTask;
@@ -1021,7 +1021,7 @@ public class AssignmentActivity extends AppCompatActivity {
         TextView tvDialogCorrect = (TextView) layout.findViewById(R.id.tvDialogCorrect);
         TextView tvDialogCorrectAverage = (TextView) layout.findViewById(R.id.tvDialogCorrectAverage);
         final List<Map<String, String>> questionList = new ArrayList<>();
-        SomethingElseEntirely questionListAdapter = new SomethingElseEntirely(this,questionList);
+        QuestionListAdapter questionListAdapter = new QuestionListAdapter(this,questionList);
         lvDialogQuestions.setAdapter(questionListAdapter);
 
         chartDialog.setUsePercentValues(true);
