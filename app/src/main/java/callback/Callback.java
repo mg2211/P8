881db3f7fe7ed91d305f3c33a917b4453958dc7a
EmilSiegenfeldt <siegen@siegen.dk>
2016-05-23 16:01:56 +0200

    More commenting

--- app/src/main/java/callback/Callback.java
+++ app/src/main/java/callback/Callback.java
@@ -5,7 +5,12 @@ import java.util.HashMap;
 /**
  * Created by ida803f16
  */
+/*Interface Callback
+* Used when passing async results back to caller activity*/
 public interface Callback {
-
+    /**
+     *
+     * @param asyncResults - the HashMap of results
+     */
     void asyncDone (HashMap<String, HashMap<String, String>> asyncResults);
 }
--- app/src/main/java/callback/DatePickerCallback.java
+++ app/src/main/java/callback/DatePickerCallback.java
@@ -1,6 +1,13 @@
 package callback;
 
-
+/**
+ * Interface DatePickerCallback
+ * Used for passing date from the datepicker dialog to caller activity
+ */
 public interface DatePickerCallback {
+    /**
+     *
+     * @param timestamp unix timestamp
+     */
     void dateSelected(Long timestamp);
 }
