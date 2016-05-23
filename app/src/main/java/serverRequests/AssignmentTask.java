20c6f68ef7032fd70bde139b652d8c9e7d314ef2
EmilSiegenfeldt <siegen@siegen.dk>
2016-05-23 17:26:09 +0200

    Commenting done!

--- app/src/main/java/serverRequests/AssignmentTask.java
+++ app/src/main/java/serverRequests/AssignmentTask.java
@@ -27,44 +27,79 @@ import java.util.HashMap;
 /**
  * Created by ida803f16
  */
+
+/**
+ * AssignmentTask
+ * Used for assigning, deleting and reading and updating assignments for students
+ */
 public class AssignmentTask extends AsyncTask<String, Void, HashMap<String, HashMap<String, String>>> {
 
+    /*Callback interface*/
     private final Callback delegate;
+    /*Declaring a ProgressDialog*/
     private final ProgressDialog progressDialog;
+    /*The caller activity*/
     private final Context context;
 
+    /**
+     * Constructor
+     * @param delegate - The Callback interface
+     * @param context - The Caller activity
+     */
     public AssignmentTask(Callback delegate, Context context){
         this.delegate = delegate;
         this.context = context;
+        /*Creating and setting a progressdialog*/
         progressDialog = new ProgressDialog(context);
         progressDialog.setCancelable(false);
         progressDialog.setTitle("Processing...");
         progressDialog.setMessage("Please wait...");
         progressDialog.show();
     }
+
+    /**
+     * Helper method
+     * @param method - The method to send the server
+     * @param studentId - The studentID
+     * @param assignmentLibId - The AssignmentLibraryId
+     * @param from - Avaiable from timestamp
+     * @param to - Avaiable to timestamp
+     * @param assignmentId - The assignmentId
+     */
     public void executeTask(String method, String studentId, String assignmentLibId, String from, String to, String assignmentId){
         this.execute(method, studentId, assignmentLibId, from, to, assignmentId);
     }
 
+    /**
+     * doInBackground handles the connection to the server
+     * @param params - String array of parameters either sent from the executeTask method or directly to the class
+     * @return HashMap of results
+     */
     @Override
     protected HashMap<String, HashMap<String, String>> doInBackground(String... params) {
 
+        /*Getting params*/
         String method = params[0];
         String studentId = params[1];
         String assignmentLibId = params[2];
         String from = params[3];
         String to = params[4];
         String id = params[5];
+
+        /*Initiating response variables*/
         String generalResponse = null;
         int responseCode = 0;
 
+        /*Creating the results HashMap to be returned*/
         HashMap<String, HashMap<String, String>> results = new HashMap<>();
 
         try {
+            /*Creating the actual server call*/
             URL url = new URL("http://emilsiegenfeldt.dk/p8/assignments.php");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
 
+            /*Building URI with POST parameters*/
             Uri.Builder builder = new Uri.Builder().appendQueryParameter("studentId", studentId)
                     .appendQueryParameter("assignmentlibraryid",assignmentLibId)
                     .appendQueryParameter("method",method)
@@ -82,20 +117,28 @@ public class AssignmentTask extends AsyncTask<String, Void, HashMap<String, Hash
 
             connection.connect();
 
-            //Catch server response
+            /*Catch server response*/
             InputStream in = new BufferedInputStream(connection.getInputStream());
 
-            String response = IOUtils.toString(in, "UTF-8"); //convert to readable string
-            //convert to JSON object
-            Log.d("response",response);
+            String response = IOUtils.toString(in, "UTF-8");
 
+            /*convert to JSON object*/
             JSONObject JSONResult = new JSONObject(response);
+            /*Getting response from the JSON*/
             generalResponse = JSONResult.getString("generalResponse");
             responseCode = JSONResult.getInt("responseCode");
+            /*If method is "get", parse the JSON to get assignments*/
+
+
             if(method.equals("get")) {
+                /*Get the array of assignments from JSON*/
                 JSONArray assignments = JSONResult.getJSONArray("assignments");
+                /*Iterate the array*/
                 for (int i = 0; i < assignments.length(); i++) {
+                    /*Getting the specific assignment in the array*/
                     JSONObject specificAssignment = assignments.getJSONObject(i);
+
+                    /*Getting all information from the array*/
                     String assLibId = String.valueOf(specificAssignment.getInt("assignmentlibraryid"));
                     String assignmentId = String.valueOf(specificAssignment.getInt("id"));
                     String assignmentName = specificAssignment.getString("assignmentName");
@@ -106,6 +149,7 @@ public class AssignmentTask extends AsyncTask<String, Void, HashMap<String, Hash
                     String isComplete = specificAssignment.getString("isComplete");
                     String timeSpent = specificAssignment.getString("timeSpent");
 
+                    /*Creating a map for containing information and put data in it*/
                     HashMap<String, String> assignmentInfo = new HashMap<>();
                     assignmentInfo.put("assignmentlibraryid", assLibId);
                     assignmentInfo.put("assignmentid", assignmentId);
@@ -117,6 +161,7 @@ public class AssignmentTask extends AsyncTask<String, Void, HashMap<String, Hash
                     assignmentInfo.put("isComplete", isComplete);
                     assignmentInfo.put("timeSpent",timeSpent);
 
+                    /*Put the map created into the results HashMap*/
                     results.put("AssignmentId: " + assignmentId, assignmentInfo);
                 }
             }
@@ -128,29 +173,40 @@ public class AssignmentTask extends AsyncTask<String, Void, HashMap<String, Hash
             e.printStackTrace();
         }
 
+        /*Creating a HashMap for containing the response variables*/
         HashMap<String, String> response = new HashMap<>();
+        /*Putting the response in the response map*/
         response.put("generalResponse", generalResponse);
         response.put("responseCode", String.valueOf(responseCode));
+        /*Putting the response map into the results HashMap*/
         results.put("response", response);
 
         return results;
     }
+
+    /**
+     * Handles the results from doInBackground
+     * @param results returned from doInBackground
+     */
     protected void onPostExecute(HashMap<String, HashMap<String, String>> results) {
+        /*getting the response variables*/
         String generalResponse = results.get("response").get("generalResponse");
         String responseCode = results.get("response").get("responseCode");
+        /*Dismisses the progressdialog*/
         progressDialog.dismiss();
 
         if (Integer.parseInt(responseCode) == 100) {
-            //everything Okay
+            /*Everything okay - removing the reponse from the results map*/
             results.remove("response");
+            /*Sending the result back to the caller activity via the Callback interface*/
             delegate.asyncDone(results);
         } else if (Integer.parseInt(responseCode) == 200) {
-            //Something went wrong database side
+            /*Something went wrong database side, show a toast saying so*/
             int duration = Toast.LENGTH_LONG;
             Toast toast = Toast.makeText(context, generalResponse, duration);
             toast.show();
         } else if (Integer.parseInt(responseCode) == 300) {
-            //Server connection error
+            /*If the doInBackground fails, a toast saying so is shown*/
             int duration = Toast.LENGTH_LONG;
             CharSequence alert = "Server connection failed - Please try again later";
             Toast toast = Toast.makeText(context, alert, duration);
--- app/src/main/java/serverRequests/TextTask.java
+++ app/src/main/java/serverRequests/TextTask.java
@@ -27,37 +27,70 @@ import java.util.HashMap;
 /**
  * Created by ida803f16
  */
-public class TextTask extends AsyncTask<String, Void, HashMap<String, HashMap<String, String>>> {
 
+/**
+ * TextTask
+ * Used for creating, reading, updating and deleting texts
+ */
+public class TextTask extends AsyncTask<String, Void, HashMap<String, HashMap<String, String>>> {
+    /*The caller activity*/
     private final Context context;
+    /*The Callback Interface*/
     private final Callback delegate;
+    /*Declaring a Progressdialog*/
     private final ProgressDialog progressDialog;
 
+    /**
+     * Constructor
+     * @param delegate - The callback interface to be used
+     * @param context - The caller activity
+     */
     public TextTask(Callback delegate, Context context){
         this.delegate = delegate;
         this.context = context;
+        /*Creating and setting the progressdialog*/
         progressDialog = new ProgressDialog(context);
         progressDialog.setCancelable(false);
         progressDialog.setTitle("Processing...");
         progressDialog.setMessage("Please wait ...");
         progressDialog.show();
     }
+
+    /**
+     * Helper method
+     * @param method - The method to send the server
+     * @param textId - The id of the text
+     * @param textName - The name of the text
+     * @param textContent - The content of the text
+     * @param complexity - The complexity of the text
+     */
     public void executeTask(String method, String textId, String textName, String textContent, double complexity){
         this.execute(method, textId, textName, textContent, String.valueOf(complexity));
     }
+
+    /**
+     * doInBackground handles the server call
+     * @param params - String array, either sent from the executeTask method or directly to the class
+     * @return HashMap of results
+     */
     @Override
     protected HashMap <String, HashMap<String, String>> doInBackground(String... params) {
+
+        /*Getting params*/
         String method = params[0];
         String textId = params[1];
         String textName = params[2];
         String textContent = params[3];
         String complexity = params[4];
 
+        /*Creating results and response maps*/
         HashMap<String, HashMap<String, String>> results = new HashMap<>();
         HashMap<String, String> response = new HashMap<>();
         try {
+            /*Creating the server call*/
             URL url = new URL("http://emilsiegenfeldt.dk/p8/texts.php");
 
+            /*Building the URI with POST parameters*/
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             Uri.Builder builder = new Uri.Builder().appendQueryParameter("method", method)
@@ -76,36 +109,50 @@ public class TextTask extends AsyncTask<String, Void, HashMap<String, HashMap<St
             os.close();
             connection.connect();
 
+            /*Catches the server response*/
             InputStream in = new BufferedInputStream(connection.getInputStream());
 
             String serverResponse = IOUtils.toString(in, "UTF-8");
-            if(method.equals("update")){
-                Log.d("SERVER RESPONSE",serverResponse);
-            }
 
+            /*Converts the server response to JSONObject */
             JSONObject JSONResult = new JSONObject(serverResponse);
+
+            /*Getting the reponse from JSON*/
             String generalResponse = JSONResult.getString("generalResponse");
             String responseCode = String.valueOf(JSONResult.getInt("responseCode"));
 
+            /*Putting the response into it's HashMap*/
             response.put("generalResponse", generalResponse);
             response.put("responseCode", responseCode);
+
+            /*If the method is create, get the text's new id and put it in the response*/
             if(method.equals("create")){
                 response.put("insertedId", JSONResult.getString("insertedId"));
             }
 
+            /*If the method is "get", parse the JSON for texts*/
             if(method.equals("get")){
+                /*Getting the array of texts*/
                 JSONArray texts = JSONResult.getJSONArray("texts");
+                /*Iterate the array of texts*/
                 for (int i = 0; i < texts.length(); i++) {
+                    /*Getting the specific text in the array*/
                     JSONObject specificText = texts.getJSONObject(i);
+
+                    /*Getting all information from the array*/
                     String specificTextname = specificText.getString("textName");
                     String specificTextContent = specificText.getString("textContent");
                     String specificTextId = specificText.getString("textId");
                     double specificTextComplexity = specificText.getDouble("complexity");
+
+                    /*Creating a HashMap for containing textinfo and put data in it*/
                     HashMap<String, String> textInfo = new HashMap<>();
                     textInfo.put("textname", specificTextname);
                     textInfo.put("textcontent", specificTextContent);
                     textInfo.put("id", specificTextId);
                     textInfo.put("complexity", String.valueOf(specificTextComplexity));
+
+                    /*Putting a text into the results map*/
                     results.put("text"+i,textInfo);
                 }
             }
@@ -116,16 +163,24 @@ public class TextTask extends AsyncTask<String, Void, HashMap<String, HashMap<St
         } catch (JSONException e) {
             e.printStackTrace();
         }
+        /*Putting the response into the results map*/
         results.put("response", response);
         return results;
     }
 
+    /**
+     * Handles the results from doInBackground
+     * @param results returned from doInBackground
+     */
     protected void onPostExecute (HashMap<String, HashMap<String, String>> results){
+        /*Dismisses the progressdialog*/
         progressDialog.dismiss();
+        /*Showing a toast with the generalResponse*/
         int duration = Toast.LENGTH_LONG;
         CharSequence alert = results.get("response").get("generalResponse");
         Toast toast = Toast.makeText(context, alert, duration);
         toast.show();
+        /*Sending the results back to the caller activity via the Callback interface*/
         delegate.asyncDone(results);
     }
 }
